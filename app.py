2cbf1c25204f4c5bb3cca82fe1c2b467dbfdb515
panktipatel2007 <patelpankti3201@gmail.com>
2026-06-25 13:54:11 +0530

    Update app to run on specified port from environment

--- app.py
+++ app.py
@@ -25,5 +25,8 @@ def analyze():
     }
 
     return render_template('index.html', result=feedback)
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
